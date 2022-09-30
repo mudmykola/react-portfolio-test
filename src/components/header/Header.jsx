9e59c843482ee75c3f7d83b74234b596ab2472f7
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-09-30 23:41:45 +0300

    done=>dev+adapt[header~cta~header-social]

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/img/me.png'
+import HeaderSocial from './HeaderSocial'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header-box">
+        <h5>Hello I`am</h5>
+        <h1>Mykola Mud</h1>
+        <h5 className='text-light'>Junior Front-End Engineer</h5>
+        <CTA/>
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a className='scroll-down' href="#contact">Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
