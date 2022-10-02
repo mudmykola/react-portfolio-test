ce06076ab55712d827f33e077ceda91ec2d23f43
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-10-02 14:56:35 +0300

    feat->add~[portfolio card massive]

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,6 +7,51 @@ import profImg4 from "../../assets/img/prof-img-4.png";
 import profImg5 from "../../assets/img/prof-img-5.png";
 import profImg6 from "../../assets/img/prof-img-6.png";
 
+const data = [
+  {
+    id: 1,
+    image: profImg1,
+    title: "Landing 'Oxford Game'",
+    github: "https://github.com/mudmykola/game-oxford",
+    demo: "http://game-oxford.surge.sh/",
+  },
+  {
+    id: 2,
+    image: profImg2,
+    title: "Landing page 'Burger'",
+    github: "https://github.com/mudmykola/lending-burger-2.0",
+    demo: "http://landing-burger.surge.sh/",
+  },
+  {
+    id: 3,
+    image: profImg3,
+    title: "Landing page 'Mud web'",
+    github: "https://github.com/mudmykola/mud-page",
+    demo: "https://mudmykola.github.io/mud-page/",
+  },
+  {
+    id: 4,
+    image: profImg4,
+    title: "Landing 'Seller pamp'",
+    github: "https://github.com/mudmykola/seller-pump",
+    demo: "http://seller-pump.surge.sh/",
+  },
+  {
+    id: 5,
+    image: profImg5,
+    title: "Landing page 'Spider'",
+    github: "https://github.com/mudmykola/spider-landing",
+    demo: "http://spider-landing.surge.sh/",
+  },
+  {
+    id: 6,
+    image: profImg6,
+    title: "Landing page 'Mud web'",
+    github: "https://github.com/mudmykola/vue-task-manager",
+    demo: "http://vue-task-manager.surge.sh/",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -14,138 +59,24 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio-box">
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg1} alt="prof-img-1" />
-          </div>
-          <h3>Landing 'Oxford Game'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/game-oxford"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="http://game-oxford.surge.sh/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg2} alt="prof-img-2" />
-          </div>
-          <h3>Landing page 'Burger'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/lending-burger-2.0"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="http://landing-burger.surge.sh/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg3} alt="prof-img-3" />
-          </div>
-          <h3>Landing page 'Mud web'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/mud-page"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="https://mudmykola.github.io/mud-page/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg4} alt="prof-img-4" />
-          </div>
-          <h3>Landing 'Seller pamp'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/seller-pump"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="http://seller-pump.surge.sh/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg5} alt="prof-img-5" />
-          </div>
-          <h3>Landing page 'Spider'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/spider-landing"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="http://spider-landing.surge.sh/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio-item">
-          <div className="portfolio-item__image">
-            <img src={profImg6} alt="prof-img-6" />
-          </div>
-          <h3>Landing 'Task manager'</h3>
-          <div className="portfolio-item__cta">
-            <a
-              className="btn"
-              target="blank"
-              href="https://github.com/mudmykola/vue-task-manager"
-            >
-              GitHub
-            </a>
-            <a
-              className="btn btn-primary"
-              target="blank"
-              href="http://vue-task-manager.surge.sh/"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio-item">
+              <div className="portfolio-item__image">
+                <img src={image} alt="{title}" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio-item__cta">
+                <a className="btn" target="blank" href={github}>
+                  GitHub
+                </a>
+                <a className="btn btn-primary" target="blank" href={demo}>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
