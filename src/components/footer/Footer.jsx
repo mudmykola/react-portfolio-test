ba25475521a6c18cc1b41cebb25c16c9f5df050d
Mykola <46628437+mudmykola@users.noreply.github.com>
2022-10-03 23:02:06 +0300

    rearrange -> dev+adapt [footer]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer-logo">
+        MUD
+      </a>
+      <ul className="footer-list">
+        <li>
+          <a href="#">Home</a>
+          <a href="#about">About</a>
+          <a href="#experience">Experience</a>
+          <a href="#services">Services</a>
+          <a href="#portfolio">Portfolio</a>
+          <a href="#review">Review</a>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer-socials">
+        <a href="https://www.linkedin.com/in/mud-developer/">
+          <FaLinkedinIn />
+        </a>
+        <a href="https://www.instagram.com/mud.web.developer">
+          <FaInstagram />
+        </a>
+      </div>
+      <div className="footer-copy">
+        <p>&copy; MUD Web Portfolio. All right reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
